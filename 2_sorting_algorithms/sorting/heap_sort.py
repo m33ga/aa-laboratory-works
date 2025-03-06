ff59ce3f3a0d58a0d33a4eb98655dce9fd4e8067
Mihai Gurduza <mihai.gurduza@isa.utm.md>
2025-03-06 18:03:20 +0200

    add heap sort algo

---
+++ 2_sorting_algorithms/sorting/heap_sort.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+def heap_sort(arr):
+    def heapify(n, i):
+        largest = i
+        left = 2 * i + 1
+        right = 2 * i + 2
+        if left < n and arr[left] > arr[largest]:
+            largest = left
+        if right < n and arr[right] > arr[largest]:
+            largest = right
+        if largest != i:
+            arr[i], arr[largest] = arr[largest], arr[i]
+            heapify(n, largest)
+
+    n = len(arr)
+    # build maxheap
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(n, i)
+    for i in range(n - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        heapify(i, 0)
+    return arr
+
