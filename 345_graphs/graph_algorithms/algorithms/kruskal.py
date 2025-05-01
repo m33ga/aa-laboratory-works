d65ae5e12b89f71ecf00248823598f7cf304ce1b
Mihai Gurduza <mihai.gurduza@isa.utm.md>
2025-05-01 21:49:47 +0300

    perf: use index based lists for parent and rank in kruskal's algorithm

--- 345_graphs/graph_algorithms/algorithms/kruskal.py
+++ 345_graphs/graph_algorithms/algorithms/kruskal.py
@@ -1,15 +1,30 @@
 def kruskal(graph):
-    parent = {}
-    rank = {}
+    edges = []
+    node_to_idx = {}
+    idx = 0
+    for u in graph:
+        if u not in node_to_idx:
+            node_to_idx[u] = idx
+            idx += 1
+        for v, w in graph[u].items():
+            if v not in node_to_idx:
+                node_to_idx[v] = idx
+                idx += 1
+            if u < v:
+                edges.append((w, u, v))
+    edges.sort()
 
-    def find(u):
-        while parent[u] != u:
-            parent[u] = parent[parent[u]]
-            u = parent[u]
-        return u
+    parent = list(range(idx))
+    rank = [0] * idx
 
-    def union(u, v):
-        ru, rv = find(u), find(v)
+    def find(u_idx):
+        while parent[u_idx] != u_idx:
+            parent[u_idx] = parent[parent[u_idx]]
+            u_idx = parent[u_idx]
+        return u_idx
+
+    def union(u_idx, v_idx):
+        ru, rv = find(u_idx), find(v_idx)
         if ru == rv:
             return False
         if rank[ru] > rank[rv]:
@@ -20,18 +35,13 @@ def kruskal(graph):
                 rank[rv] += 1
         return True
 
-    edges = []
-    for u in graph:
-        for v, w in graph[u]:
-            if u < v:
-                edges.append((w, u, v))
+    mst_edges = []
+    total_weight = 0
+    for w, u, v in edges:
+        u_idx = node_to_idx[u]
+        v_idx = node_to_idx[v]
+        if union(u_idx, v_idx):
+            mst_edges.append((u, v, w))
+            total_weight += w
 
-    for node in graph:
-        parent[node] = node
-        rank[node] = 0
-
-    mst = []
-    for weight, u, v in sorted(edges):
-        if union(u, v):
-            mst.append((u, v, weight))
-    return mst
+    return mst_edges, total_weight
