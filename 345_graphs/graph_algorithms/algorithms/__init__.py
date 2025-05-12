1088125a32f6dfbf0ef0913c2c7e59e075cd9e27
Mihai Gurduza <mihai.gurduza@isa.utm.md>
2025-05-12 21:52:33 +0300

    rename algorithm for shorter name in table header

--- 345_graphs/graph_algorithms/algorithms/__init__.py
+++ 345_graphs/graph_algorithms/algorithms/__init__.py
@@ -1,8 +1,8 @@
-from .floyd_warshall import floyd_warshall
+from .floyd_warshall import floyd
 from .prim import prim
 from .bfs import bfs
 from .kruskal import kruskal
 from .dfs import dfs
 from .dijkstra import dijkstra
 
-__all__ = ["floyd_warshall", "prim", "bfs", "kruskal", "dfs", "dijkstra"]
+__all__ = ["floyd", "prim", "bfs", "kruskal", "dfs", "dijkstra"]
